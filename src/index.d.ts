547062e23c4d5cca7f0d17f865c75b98ce68c8d4
RianDoty <316071@students.cnusd.k12.ca.us>
2022-10-27 08:08:25 -0700

    cool beans

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,15 @@
+//Static asset imports
 declare module '*.png' {
+    const url: string
+    export default url
+}
+
+declare module '*.mp3' {
+    const url: string
+    export default url
+}
+
+declare module '*.m4a' {
     const url: string
     export default url
 }
